00792a00227eb83f5b33fc096bd47733f2912955
michaelk99 <4404414+michaelk99@users.noreply.github.com>
2025-11-27 16:04:10 +0100

    librelane/run_librelane.sh: update PDK

--- librelane/run_librelane.sh
+++ librelane/run_librelane.sh
@@ -3,15 +3,15 @@
 # =====================================================
 # Author: Simon Dorrer
 # Last Modified: 02.10.2025
-# Description: This .sh file switches to the SKY130 PDK, runs the LibreLane flow and opens the layout in the OpenROAD GUI.
+# Description: This .sh file switches to the GF180 PDK, runs the LibreLane flow and opens the layout in the OpenROAD GUI.
 # =====================================================
 
 set -e -x
 
 cd $(dirname "$0")
 
-# Switch to sky130A PDK
-source sak-pdk-script.sh sky130A sky130_fd_sc_hd > /dev/null
+# Switch to gf180mcuD PDK
+source sak-pdk-script.sh gf180mcuD gf180mcu_fd_sc_mcu7t5v0 > /dev/null
 
 # Run LibreLane
 librelane --manual-pdk config.json
